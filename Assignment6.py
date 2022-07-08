c798a2f93907248f55442df2a905241bc88a1905
bt-21107001 <103369413+bt-21107001@users.noreply.github.com>
2022-07-09 00:12:44 +0530

    Update Assignment6.py

--- Assignment6.py
+++ Assignment6.py
@@ -113,27 +113,29 @@ print()
 # In[ ]:
 
 
-def findTriplets(arr, n):
+class Zero:
+    def threeSum(self, nums):
+        nums, result, i = sorted(nums), [], 0
+        while i < len(nums) - 2:
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+            i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+                i += 1
+        return result
 
-	found = False
-	for i in range(0, n-2):
-	
-		for j in range(i+1, n-1):
-		
-			for k in range(j+1, n):
-			
-				if (arr[i] + arr[j] + arr[k] == 0):
-					print(arr[i], arr[j], arr[k])
-					found = True
-if (found == False):
-    print("No triplets exist")
-lst = []
-n = int(input("Enter number of elements : "))
-
-for i in range(0, n):
-	ele = int(input())
-	lst.append(ele) 
-print(findTriplets(lst,n))                
+print(Zero().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]))                 
 
 # In[ ]:
 
